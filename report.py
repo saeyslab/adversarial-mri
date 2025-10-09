890121a4f6656252bb3dd811f5b316be8496a31d
jpeck <jonathan.peck@ugent.be>
2025-10-09 15:49:51 +0200

    reporting

--- report.py
+++ report.py
@@ -15,32 +15,42 @@ parser.add_argument('-out', type=str, default='./out', help='output directory')
 args = parser.parse_args()
 
 # compute metrics
-scores = []
-labels = []
 summaries = {
-    'data': [],
-    'mean': [],
-    'std': []
+    'total_residual': [],
+    'masked_residual': [],
+    'unmasked_residual': [],
+    'total_residual_tgt': [],
+    'masked_residual_tgt': [],
+    'unmasked_residual_tgt': [],
 }
+labels = []
+scores = []
 for organ in ['knee', 'brain']:
     for coil in ['sc', 'mc']:
         for shape in ['line', 'square']:
             csvpath = Path(args.out) / f"{coil}_{organ}" / shape / "scores.csv"
             if csvpath.exists():
                 data = pd.read_csv(csvpath)
-                mu, sigma = data['metric'].mean(), data['metric'].std()
+                scores.append(data)
+
+                for k in summaries.keys():
+                    summaries[k].append(f"{data[k].mean():.2f} ({data[k].std():.2f})")
 
                 label = f"{organ} ({coil}) - {shape}"
-
-                summaries['data'].append(label)
-                summaries['mean'].append(mu)
-                summaries['std'].append(sigma)
-                scores.append(data['metric'])
                 labels.append(label)
 
 df = pd.DataFrame(summaries)
 print(tabulate(df, headers='keys', tablefmt='psql'))
 
-plt.boxplot(scores, tick_labels=labels)
+for i in range(len(scores)):
+    xs = scores[i]['masked_residual'] / scores[i]['total_residual']
+    ys = scores[i]['masked_residual_tgt'] / scores[i]['total_residual_tgt']
+
+    plt.scatter(xs, ys, label=labels[i])
+
+plt.xlim(0, 1)
 plt.ylim(0, 1)
+plt.xlabel('ground truth')
+plt.ylabel('target')
+plt.legend()
 plt.show()
