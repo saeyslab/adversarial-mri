99da9e5940274ca621cbc94f321cf41c52edf80f
jpeck <jonathan.peck@ugent.be>
2025-10-10 12:47:57 +0200

    reporting

--- report.py
+++ report.py
@@ -13,6 +13,9 @@ from tabulate import tabulate
 # parser arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-out', type=str, default='./out', help='output directory')
+parser.add_argument('-organ', type=str, default='knee', choices=['knee', 'brain'])
+parser.add_argument('-coil', type=str, default='sc', choices=['sc', 'mc'], help='single-coil (sc) or multi-coil (mc)')
+parser.add_argument('-shape', type=str, default='line', choices=['line', 'square'], help='artefact type')
 
 args = parser.parse_args()
 
@@ -25,36 +28,24 @@ summaries = {
     'masked_residual_tgt': [],
     'unmasked_residual_tgt': [],
 }
-labels = []
-scores = []
-for organ in ['knee', 'brain']:
-    for coil in ['sc', 'mc']:
-        for shape in ['line', 'square']:
-            csvpath = Path(args.out) / f"{coil}_{organ}" / shape / "scores.csv"
-            if csvpath.exists():
-                data = pd.read_csv(csvpath)
-                scores.append(data)
+csvpath = Path(args.out) / f"{args.coil}_{args.organ}" / args.shape / "scores.csv"
+if csvpath.exists():
+    data = pd.read_csv(csvpath)
 
-                for k in summaries.keys():
-                    err = 1.96*data[k].std()/np.sqrt(len(data))
-                    summaries[k].append(f"{data[k].mean():.2f} ({err:.2f})")
+    for k in summaries.keys():
+        err = 1.96*data[k].std()/np.sqrt(len(data))
+        summaries[k].append(f"{data[k].mean():.2f} ({err:.2f})")
 
-                label = f"{organ} ({coil}) - {shape}"
-                labels.append(label)
+    df = pd.DataFrame(summaries)
+    print(tabulate(df, headers='keys', tablefmt='psql'))
 
-df = pd.DataFrame(summaries)
-df['name'] = labels
-print(tabulate(df, headers='keys', tablefmt='psql'))
+    xs = data['unmasked_residual'] / data['total_residual']
+    ys = data['masked_residual_tgt'] / data['total_residual_tgt']
+    plt.scatter(xs, ys)
 
-for i in range(len(scores)):
-    xs = scores[i]['masked_residual'] / scores[i]['total_residual']
-    ys = scores[i]['masked_residual_tgt'] / scores[i]['total_residual_tgt']
-
-    plt.scatter(xs, ys, label=labels[i])
-
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-plt.xlabel('ground truth')
-plt.ylabel('target')
-plt.legend()
-plt.show()
+    plt.title(f"{args.organ} ({args.coil}) - {args.shape}")
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.xlabel('ground truth')
+    plt.ylabel('target')
+    plt.show()
